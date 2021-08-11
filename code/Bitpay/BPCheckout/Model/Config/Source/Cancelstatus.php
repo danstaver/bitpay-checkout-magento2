85526015c7e0040de4c50a68cfe45b2a5cb44205
Dan Staver <dan.staver@dish.com>
2021-08-11 11:39:19 -0600

    Deprecated interface \Magento\Framework\Option\ArrayInterface has been replaced with \Magento\Framework\Data\OptionSourceInterface.

--- code/Bitpay/BPCheckout/Model/Config/Source/Cancelstatus.php
+++ code/Bitpay/BPCheckout/Model/Config/Source/Cancelstatus.php
@@ -3,21 +3,21 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Bitpay\BPCheckout\Model\Config\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
 
 /**
  *Refund Status Model
  */
-class Cancelstatus implements \Magento\Framework\Option\ArrayInterface
+class Cancelstatus implements OptionSourceInterface
 {
-
-    public function toOptionArray()
+    public function toOptionArray(): array
     {
-
         return [
             ['value' => 'cancel', 'label' => __('True')],
             ['value' => 'ignore', 'label' => __('False')]
         ];
-
     }
 }
