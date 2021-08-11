0fee9e984e7a49ae94fdf98c16a6db0bb7ef14b9
Dan Staver <dan.staver@dish.com>
2021-08-11 10:14:25 -0600

    No params needed

--- code/Bitpay/BPCheckout/Api/IpnManagementInterface.php
+++ code/Bitpay/BPCheckout/Api/IpnManagementInterface.php
@@ -7,17 +7,16 @@ interface IpnManagementInterface
 
     /**
      * POST for ipn api
-     * @param string $param
+     *
      * @return string
      */
     public function postIpn();
 
      /**
      * POST for close api
-     * @param string $param
+      *
      * @return string
      */
     public function postClose();
 
-    
 }
